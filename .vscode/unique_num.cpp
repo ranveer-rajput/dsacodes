5f83ab05a6ec5f5b28c90ba6731b31868ca21598
ranveer-rajput <106472903+ranveer-rajput@users.noreply.github.com>
2023-09-09 10:20:44 +0530

    find uniqnum in iist

---
+++ .vscode/unique_num.cpp
@@ -0,0 +1,22 @@
+   
+// #include <iostream>
+
+// using namespace std;
+
+// int uniqNum(int arr[],int size){
+//     int ans=0;
+//     for(int i=0;i<size;i++){
+//         ans=ans^arr[i];
+//     }
+//     return ans;
+// }
+
+// int main()
+// {
+//     int arr[7]={10,20,10,30,40,30,40};
+//     int size=7;
+    
+// int ans=    uniqNum(arr,size);
+// cout<<ans;
+//     return 0;
+// }
