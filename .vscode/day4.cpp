dca25943e45a2f8bc1e4554894e989bb859e9bd6
ranveer-rajput <106472903+ranveer-rajput@users.noreply.github.com>
2023-09-16 17:58:09 +0530

    binary serch algo added

---
+++ .vscode/day4.cpp
@@ -0,0 +1,42 @@
+//bainary search
+ 
+#include <iostream>
+
+using namespace std;
+
+int bainarySearch(int arr[],int size,int target){
+     int start=0;
+     int end=size-1;
+     int mid=(start+end)/2;
+      while(start<=end){
+      
+          if(arr[mid]==target){
+              return mid;
+          }
+          else if(arr[mid]<target){
+              start=mid+1;
+          }
+          else if(arr[mid]>target){
+              end=mid-1;
+          }
+         mid=(start+end)/2;
+      }
+     return -1;
+}
+
+int main()
+{
+   int arr[]={10,20,30,40,50,60,70,80,90};
+   int size=9;
+   int target=90;
+ int result=  bainarySearch(arr,size,target);
+   if(result==-1){
+       cout<<"target not found";
+       
+   }
+   else{
+       cout<<"target found at:"<<result;
+   }
+
+    return 0;
+}
