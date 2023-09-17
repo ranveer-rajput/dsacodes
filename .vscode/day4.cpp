d88353b54116fbbf33085a3f47f238d2c4960bec
ranveer-rajput <106472903+ranveer-rajput@users.noreply.github.com>
2023-09-17 12:04:31 +0530

    find firstoccurance in array using binary search

--- .vscode/day4.cpp
+++ .vscode/day4.cpp
@@ -40,3 +40,50 @@ int main()
 
     return 0;
 }
+
+
+
+/******************************************************************************
+ find firstOccurance using binary search
+*******************************************************************************/
+#include <iostream>
+
+using namespace std;
+int firstAccucrance(int arr[],int size,int target){
+    int s=0;
+    int e=size-1;
+    int ans=-1;
+    while(s<=e){
+        int mid=(s+e)/2;
+        if(target==arr[mid]){
+            ans=mid;
+            e=mid-1;
+            
+        }
+        else if(target>arr[mid]){
+            s=mid+1;
+        }
+        else if(target<arr[mid]){
+            e=mid-1;
+        }
+    }
+    return ans;
+    
+}
+
+int main()
+{
+  int arr[]={10,30,30,30,40,50,60};
+  int size=7;
+  int target=30;
+ int result= firstAccucrance(arr,size,target);
+ 
+  if(result==-1){
+      cout<<"target not found";
+  }
+  else{
+      cout<<"the firstAccucrance found at:"<<result;
+  }
+
+    return 0;
+}
